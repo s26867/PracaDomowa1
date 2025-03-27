fd92d9d9dfa7da2f714960c57f25951503ac8e9f
s26867 <s26867@pjwstk.edu.pl>
2025-03-27 14:52:20 +0100

    Dodanie klasy KontenerChlodniczy.cs i KontenerNaGaz.cs

---
+++ KontenerChlodniczy.cs
@@ -0,0 +1,32 @@
+﻿namespace PracaDomowa1;
+
+public class KontenerChlodniczy : Kontener
+{
+    public string RodzajProduktu { get; private set; }
+    public double Temperatura { get; private set; }
+    public double MinimalnaTemperatura { get; private set; }
+
+    public KontenerChlodniczy(double wysokosc, double wagaWlasna, double glebokosc, double maksymalnaLadownosc, string rodzajProduktu, double minimalnaTemperatura, double temperatura)
+        : base("C", wysokosc, wagaWlasna, glebokosc, maksymalnaLadownosc)
+    {
+        RodzajProduktu = rodzajProduktu;
+        MinimalnaTemperatura = minimalnaTemperatura;
+        Temperatura = temperatura;
+
+        if (Temperatura < MinimalnaTemperatura)
+            throw new Exception($"Temperatura kontenera ({Temperatura}°C) jest zbyt niska dla produktu {RodzajProduktu} (min {MinimalnaTemperatura}°C)");
+    }
+
+    public override void ZaladujLadunek(double masa)
+    {
+        if (masa > MaksymalnaLadownosc)
+            throw new WyjatekPrzeladowania("Zbyt ciężki ładunek w kontenerze chłodniczym!");
+
+        MasaLadunku = masa;
+    }
+
+    public override void OproznijLadunek()
+    {
+        MasaLadunku = 0;
+    }
+}
---
+++ KontenerNaGaz.cs
@@ -0,0 +1,32 @@
+﻿namespace PracaDomowa1;
+
+public class KontenerNaGaz : Kontener, IPowiadamiaczZagrozenia
+{
+    public double Cisnienie { get; private set; }
+
+    public KontenerNaGaz(double wysokosc, double wagaWlasna, double glebokosc, double maksymalnaLadownosc, double cisnienie)
+        : base("G", wysokosc, wagaWlasna, glebokosc, maksymalnaLadownosc)
+    {
+        Cisnienie = cisnienie;
+    }
+
+    public override void ZaladujLadunek(double masa)
+    {
+        if (masa > MaksymalnaLadownosc)
+        {
+            PowiadomOZagrozeniu(NumerSeryjny, "Przekroczono dopuszczalną ładowność kontenera na gaz.");
+            throw new WyjatekPrzeladowania("Za dużo gazu!");
+        }
+        MasaLadunku = masa;
+    }
+
+    public override void OproznijLadunek()
+    {
+        MasaLadunku *= 0.05; // Zostaje 5%
+    }
+
+    public void PowiadomOZagrozeniu(string numerSeryjny, string wiadomosc)
+    {
+        Console.WriteLine($"[Zagrożenie - Kontener na gaz {numerSeryjny}]: {wiadomosc}");
+    }
+}
