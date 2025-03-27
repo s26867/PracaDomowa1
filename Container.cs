2096b9479d0541f8ec69bf193b4160367a6ae733
s26867 <s26867@pjwstk.edu.pl>
2025-03-27 13:33:00 +0100

    Dodanie klasy Kontener

---
+++ Container.cs
@@ -0,0 +1,27 @@
+﻿namespace PracaDomowa1;
+
+abstract class Konterer
+{
+   private static int licznikNumerow = 1;
+   public string NumerSeryjny { get; protected set; } 
+   public double MasaLadunku { get; protected set; }
+   public double Wysokosc { get; protected set; } 
+   public double WagaWlasna { get; protected set; } 
+   public double Glebokosc { get; protected set; } 
+   public double MaksymalnaLadownosc { get; protected set; }
+   
+   //konstruktor
+   protected Kontener(string typ, double wysokosc, double wagaWlasna, double glebokosc, double maksymalnaLadownosc)
+   {
+      NumerSeryjny = $"KON-{typ}-{licznikNumerow++}";
+      Wysokosc = wysokosc;
+      WagaWlasna = wagaWlasna;
+      Glebokosc = glebokosc;
+      MaksymalnaLadownosc = maksymalnaLadownosc;
+   }
+
+   public abstract void ZaladujLadunek(double masa);
+   public abstract void OproznijLadunek();
+
+   public override string ToString() => $"Kontener {NumerSeryjny}: MasaLadunku={MasaLadunku}, MaksymalnaLadownosc={MaksymalnaLadownosc}";
+}
